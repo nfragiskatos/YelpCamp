7ef6a2455ac4866206494c97d32b3ee9cc02dde8
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-22 15:45:54 +0000

    added new.ejs that has form to create new campground. added /campgrounds post route to handle the form call. Add /campgrounds/new route to direct to the form.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
@@ -14,6 +17,17 @@ app.get("/", function(req, res) {
 
 app.get("/campgrounds", function(req, res) {
      res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+    // get data from form
+    res.send("You hit post route");
+    //add to campgrounds array
+    // redirect back to campgrounds page
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new"); 
 });
 
 
