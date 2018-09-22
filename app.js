ffafb2ba3683b16c311166020dd857e1697bdeab
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-22 04:44:45 +0000

    inital setup

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log("YelpCamp server has started!"); 
+});
