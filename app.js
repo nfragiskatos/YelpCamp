0aa387bf8d36fc46e913901a4a84dbfe0013db7c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-23 02:04:45 +0000

    changed the /campgrounds GET request to pull from the Mongo DB instead of the hard coded array

--- app.js
+++ app.js
@@ -15,42 +15,34 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-    {
-    name: "Temp Campground 222222", 
-    image: "https://farm3.staticflickr.com/2763/4184577481_f1fdcb4252.jpg"
+// Campground.create(
+//     {
+//     name: "Temp Campground 222222", 
+//     image: "https://farm3.staticflickr.com/2763/4184577481_f1fdcb4252.jpg"
     
-    }, function(err, campground) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("NEWLY CREATED CAMPGROUND");
-            console.log(campground);
-        }
-    }
-);
-
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/709/23277997680_b812daf6b5.jpg"},
-        {name: "Granite Hill", image: "https://farm3.staticflickr.com/2763/4184577481_f1fdcb4252.jpg"},
-        {name: "Mountain Goats Rest", image: "https://farm4.staticflickr.com/3656/3600206210_d7819721a2.jpg"},
-        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/709/23277997680_b812daf6b5.jpg"},
-        {name: "Granite Hill", image: "https://farm3.staticflickr.com/2763/4184577481_f1fdcb4252.jpg"},
-        {name: "Mountain Goats Rest", image: "https://farm4.staticflickr.com/3656/3600206210_d7819721a2.jpg"},
-        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/709/23277997680_b812daf6b5.jpg"},
-        {name: "Granite Hill", image: "https://farm3.staticflickr.com/2763/4184577481_f1fdcb4252.jpg"},
-        {name: "Mountain Goats Rest", image: "https://farm4.staticflickr.com/3656/3600206210_d7819721a2.jpg"},
-        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/709/23277997680_b812daf6b5.jpg"},
-        {name: "Granite Hill", image: "https://farm3.staticflickr.com/2763/4184577481_f1fdcb4252.jpg"},
-        {name: "Mountain Goats Rest", image: "https://farm4.staticflickr.com/3656/3600206210_d7819721a2.jpg"}
-    ];
+//     }, function(err, campground) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("NEWLY CREATED CAMPGROUND");
+//             console.log(campground);
+//         }
+//     }
+// );
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-     res.render("campgrounds", {campgrounds: campgrounds});
+    // get all campgrounds from db
+    Campground.find({}, function(err, campgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: campgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
