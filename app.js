70a238dd281ff9db4ea5e846f50e51ea04b66fd0
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-23 03:42:28 +0000

    Changed name to campgrounds.ejs to index.ejs to follow RESTful routing practices. Added a campgrounds/:id route that will route to a page to show a specific campground. Made the show.ejs template, which is where /campgrounds/:id routes to. show.ejs so far just has the name of the campground, the picture, and the description.

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+//==========================================
+// ** RESTFUL Routing Setup **
+//==========================================
+
+// name     url                 verb    desc.
+//---------------------------------------------------------------
+// INDEX    /campgrounds        GET     Display a list of all dogs
+// NEW      /campgrounds/new    GET     Displays a form to make a new dog
+// CREATE   /campgrounds        POST    Add a new dog to DB
+// SHOW     /campgrounds/:id    GET     Show info about one campground
+
+
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
@@ -10,41 +22,45 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
-//     {
-//     name: "Temp Campground 222222", 
-//     image: "https://farm3.staticflickr.com/2763/4184577481_f1fdcb4252.jpg"
-    
-//     }, function(err, campground) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log("NEWLY CREATED CAMPGROUND");
-//             console.log(campground);
+//         {
+//             name: "Granite Hill", 
+//             image: "https://www.quebecoriginal.com/en/listing/images/800x600/ae2894cf-af0a-46dc-904d-8a91b0059376/camping-parc-national-du-mont-tremblant-de-la-diable-camping-secteur-la-diable.jpg",   
+//             description: "This is a huge granite hill. No bathrooms. No water. Beautiful granite!"
+//         }, function(err, campground) {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log("Added a new campground");
+//                 console.log(campground);
+//             }
 //         }
-//     }
-// );
+//     );
+
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// INDEX route - show all campgrounds
 app.get("/campgrounds", function(req, res) {
     // get all campgrounds from db
     Campground.find({}, function(err, campgrounds) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds: campgrounds});
+            res.render("index", {campgrounds: campgrounds});
         }
     });
 });
 
+// CREATE route - add new campground to DB
 app.post("/campgrounds", function(req, res) {
     // get data from form
     var name = req.body.name;
@@ -61,21 +77,31 @@ app.post("/campgrounds", function(req, res) {
             } else {
                 console.log("Added a new campground");
                 console.log(campground);
+                // redirect back to campgrounds page
+                res.redirect("/campgrounds");
             }
         }
     );
-    
-    // redirect back to campgrounds page
-    res.redirect("/campgrounds");
 });
 
+// NEW route - show form to create campground in DB
 app.get("/campgrounds/new", function(req, res) {
     res.render("new"); 
 });
 
-
-
-
+// SHOW - Show info about one campground
+//  ** This route technically NEEDS to be under /campgrounds/new 
+//      since campgrounds/new would fit this pattern.
+app.get("/campgrounds/:id", function(req, res) {
+    // find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("YelpCamp server has started!"); 
