4081d1a8408ae795939e649cb007e6a0424c5874
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-24 03:26:06 +0000

    Refactoring: created a models directory and put in campground.js to hold all the Campgrounds db setup code. Changed app.js to import the new campgrounds.js file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //==========================================
 // ** RESTFUL Routing Setup **
 //==========================================
+// RESTful routing organizational technique/pattern to help perform CRUD
+
 
 // name     url                 verb    desc.
 //---------------------------------------------------------------
@@ -13,20 +15,14 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //         {
