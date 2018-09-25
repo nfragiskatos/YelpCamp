b2b8ba60c3b003c55ae1dca2161a403fec2456e8
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-25 23:10:54 +0000

    created the register.ejs form and added the /register GET and POST routes to app.js

--- app.js
+++ app.js
@@ -33,6 +33,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// pasport configuration
+app.use(require("express-session")({
+    secret: "Yogi is a psycho dog who is socially awkward.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res) {
     res.render("landing");
 });
@@ -130,6 +142,34 @@ app.post("/campgrounds/:id/comments", function(req, res) {
     // connect new comment to campground
     // redirect back to show page of campground we are on.
 });
+
+//========================
+// Auth Routes
+//========================
+
+//show register form
+app.get("/register", function(req, res) {
+    res.render("register"); 
+});
+
+//sign up logic
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds"); 
+        });
+    });
+});
+
+
+
+
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("YelpCamp server has started!"); 
