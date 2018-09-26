2536f573ccd260466feee13b4cb7c32516a47990
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-26 21:55:17 +0000

    Added EDIT and UPDATE routes for campgrounds. User can now click a button to go to an edit screen for the currently viewed campground, and update the information.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -54,6 +54,30 @@ router.get("/:id", function(req, res){
     });
 });
 
+// EDIT campground route
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground});         
+        }
+    });
+    
+});
+
+// UPDATE campground route
+router.put("/:id", function(req, res) {
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
