44f3b67935f7e28608c027042108aa09104bc8ed
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-27 01:56:26 +0000

    added the delete route, and added the delete button the the SHOW page for campgrounds.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -78,6 +78,17 @@ router.put("/:id", function(req, res) {
     });
 });
 
+// DESTROY campground route
+router.delete("/:id", function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
